8de64282acfcb303a5e7879da9d65ccaedc3a616
hyungin0505 <hyungin0505@naver.com>
2025-06-01 01:01:05 +0900

    Edit: 시간이 갈수록 더 많은 enemy 생성되도록

--- core/game.py
+++ core/game.py
@@ -1,4 +1,7 @@
+import random
+
 from core.button import check_play_button_clicked
+from core.debugger import d_print
 from gui.enemy import Enemy
 from utils import config
 
@@ -10,4 +13,11 @@ def game_start(w):
 
     enemies = []
     if config.game_started == 1:
-        enemies.append(Enemy(w))
+        if random.randint(0, 10-config.enemy_spawn_tick) == 0:
+            enemies.append(Enemy(w))
+            config.enemy_count += 1
+
+    if config.enemy_count == config.difficulty:
+        config.enemy_spawn_tick += 1
+        config.difficulty *= 2
+        d_print("[Game Config Update] Enemy Spawn Tick: {}, Difficlty: {}".format(config.enemy_spawn_tick, config.difficulty))
