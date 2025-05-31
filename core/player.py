141ab2ab2bb8381afd8e965bbc21e332ae0779d7
hyungin0505 <hyungin0505@naver.com>
2025-06-01 03:38:01 +0900

    Feat: Enemy 움직임 구현

--- core/player.py
+++ core/player.py
@@ -12,21 +12,24 @@ def move(w):
             if y >= 0-16:
                 y -= player_speed
             d_print("[KeyBoard Input] Pressed 'w'.")
+            d_print("[Player Moved] Player is to ({}, {}).".format(x, y))
         if w.keys['s'] and not w.keys['w']:
             y = w.getPosition(w.data.image_game_player)[1]
             if y <= SCREEN_HEIGHT-16:
                 y += player_speed
             d_print("[KeyBoard Input] Pressed 's'.")
+            d_print("[Player Moved] Player is to ({}, {}).".format(x, y))
         if w.keys['a'] and not w.keys['d']:
             x = w.getPosition(w.data.image_game_player)[0]
             if x >= 0-16:
                 x -= player_speed
             d_print("[KeyBoard Input] Pressed 'a'.")
+            d_print("[Player Moved] Player is to ({}, {}).".format(x, y))
         if w.keys['d'] and not w.keys['a']:
             x = w.getPosition(w.data.image_game_player)[0]
             if x <= SCREEN_WIDTH-16:
                 x += player_speed
             d_print("[KeyBoard Input] Pressed 'd'.")
+            d_print("[Player Moved] Player is to ({}, {}).".format(x, y))
 
         w.moveObject(w.data.image_game_player, new_x=x, new_y=y)
-        d_print("[Player Moved] Player is to ({}, {}).".format(x, y))
--- gui/enemy.py
+++ gui/enemy.py
@@ -1,29 +1,40 @@
 import random
 from utils import *
 from core.debugger import d_print
+from utils.config import difficulty
+
 
 class Enemy:
-    def __init__(self, w, x=0, y=0, speed=0, direction_x=0, direction_y=0):
-        self.speed = speed
-        self.direction_x = direction_x
-        self.direction_y = direction_y
+    def __init__(self, w, x=0, y=0, speed_x=random.randint(-difficulty*2, difficulty*2), speed_y=random.randint(-difficulty*2, difficulty*2)):
+        self.x = x
+        self.y = y
+        self.speed_x = speed_x
+        self.speed_y = speed_y
 
         edge = random.choice(['top', 'bottom', 'left', 'right'])
         if edge == 'top':
             self.x = random.randint(0-8, SCREEN_WIDTH-8)
             self.y = 0-8
+            self.speed_x = random.randint(-difficulty*2, difficulty*2)
+            self.speed_y = random.randint(difficulty, difficulty*2)
             d_print("[Enemy Spawned] Enemy is on top. ({}, {})".format(self.x, self.y))
         elif edge == 'bottom':
             self.x = random.randint(0-8, SCREEN_WIDTH-8)
             self.y = SCREEN_HEIGHT-8
+            self.speed_x = random.randint(-difficulty*2, difficulty*2)
+            self.speed_y = random.randint(-difficulty*2, -difficulty)
             d_print("[Enemy Spawned] Enemy is on bottom. ({}, {})".format(self.x, self.y))
         elif edge == 'left':
             self.x = 0-8
             self.y = random.randint(0-8, SCREEN_HEIGHT-8)
+            self.speed_x = random.randint(difficulty, difficulty*2)
+            self.speed_y = random.randint(-difficulty*2, difficulty*2)
             d_print("[Enemy Spawned] Enemy is on left. ({}, {})".format(self.x, self.y))
         elif edge == 'right':
             self.x = SCREEN_WIDTH-8
             self.y = random.randint(0-8, SCREEN_HEIGHT-8)
+            self.speed_x = random.randint(-difficulty*2, -difficulty)
+            self.speed_y = random.randint(-difficulty+2, difficulty*2)
             d_print("[Enemy Spawned] Enemy is on right. ({}, {})".format(self.x, self.y))
 
         self.image = w.newImage(
@@ -35,5 +46,15 @@ class Enemy:
             isVisible=True,
             isPixelwiseModifiable=False,
         )
-    def move(self):
-        self.x += self.direction_x
+    def move(self, w, enemies):
+        new_x = w.getPosition(self.image)[0] + self.speed_x
+        new_y = w.getPosition(self.image)[1] + self.speed_y
+
+        w.moveObject(self.image,new_x=new_x,new_y=new_y)
+
+        if -32 > w.getPosition(self.image)[0] or w.getPosition(self.image)[0] > SCREEN_WIDTH + 32 or -32 > w.getPosition(self.image)[1] or w.getPosition(self.image)[1] > SCREEN_HEIGHT + 32:
+            enemies.remove(self)
+            w.deleteObject(self.image)
+            d_print("[Enemy Event] Enemy Deleted")
+
+        d_print("[Enemy Moved] Enemy Moved to ({}, {})".format(self.x, self.y))
