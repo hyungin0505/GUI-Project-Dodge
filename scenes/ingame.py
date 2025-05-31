be1efe442a09eeceb4f9aca833ab1275b0d5dee0
hyungin0505 <hyungin0505@naver.com>
2025-06-01 01:17:38 +0900

    Edit: player 처음 위치 정중앙에 위치하도록

--- scenes/ingame.py
+++ scenes/ingame.py
@@ -2,8 +2,8 @@ from utils.config import game_started
 
 def game_player_init(w):
     w.data.image_game_player = w.newImage(
-        x = (1200-32)/2,
-        y = (800-32)/2,
+        x = (1200-32)/2-16,
+        y = (800-32)/2-16,
         filename = './assets/images/player.png',
         new_width=32,
         new_height=32,
