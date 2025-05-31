1e18f67a83c2e2eb9787f151051df09e59825f6e
hyungin0505 <hyungin0505@naver.com>
2025-06-01 01:44:45 +0900

    Edit: Player 움직임 자연스럽게

--- core/player.py
+++ core/player.py
@@ -1,17 +1,32 @@
 from core.debugger import d_print
-from utils.config import player_speed
+from utils import config
+from utils.config import player_speed, SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def move(w):
-    if w.keys['w']:
-        w.moveObject(w.data.image_game_player, new_x=w.getPosition(w.data.image_game_player)[0], new_y=w.getPosition(w.data.image_game_player)[1]-player_speed)
-        d_print("[KeyBoard Input] Pressed 'w'.")
-    elif w.keys['s']:
-        w.moveObject(w.data.image_game_player, new_x=w.getPosition(w.data.image_game_player)[0], new_y=w.getPosition(w.data.image_game_player)[1]+player_speed)
-        d_print("[KeyBoard Input] Pressed 's'.")
-    elif w.keys['a']:
-        w.moveObject(w.data.image_game_player, new_x=w.getPosition(w.data.image_game_player)[0]-player_speed, new_y=w.getPosition(w.data.image_game_player)[1])
-        d_print("[KeyBoard Input] Pressed 'a'.")
-    elif w.keys['d']:
-        w.moveObject(w.data.image_game_player, new_x=w.getPosition(w.data.image_game_player)[0]+player_speed, new_y=w.getPosition(w.data.image_game_player)[1])
-        d_print("[KeyBoard Input] Pressed 'd'.")
+    if config.game_started == 1:
+        x, y = w.getPosition(w.data.image_game_player)
+
+        if w.keys['w'] and not w.keys['s']:
+            y = w.getPosition(w.data.image_game_player)[1]
+            if y >= 0-16:
+                y -= player_speed
+            d_print("[KeyBoard Input] Pressed 'w'.")
+        if w.keys['s'] and not w.keys['w']:
+            y = w.getPosition(w.data.image_game_player)[1]
+            if y <= SCREEN_HEIGHT-16:
+                y += player_speed
+            d_print("[KeyBoard Input] Pressed 's'.")
+        if w.keys['a'] and not w.keys['d']:
+            x = w.getPosition(w.data.image_game_player)[0]
+            if x >= 0-16:
+                x -= player_speed
+            d_print("[KeyBoard Input] Pressed 'a'.")
+        if w.keys['d'] and not w.keys['a']:
+            x = w.getPosition(w.data.image_game_player)[0]
+            if x <= SCREEN_WIDTH-16:
+                x += player_speed
+            d_print("[KeyBoard Input] Pressed 'd'.")
+
+        w.moveObject(w.data.image_game_player, new_x=x, new_y=y)
+        d_print("[Player Moved] Player is to ({}, {}).".format(x, y))
