7e9be5b75bd332fa90401e45ae049328694c6f3b
hyungin0505 <hyungin0505@naver.com>
2025-05-31 21:58:55 +0900

    Refac: 버튼 관련 함수 따로 빼기

--- core/game.py
+++ core/game.py
@@ -1,10 +1,4 @@
-from core.debugger import d_print
-from utils import *
+from gui.button import check_play_button_clicked
 
 def game_start(w):
-    if w.mouse_buttons[1]:
-        d_print("Mouse 1 Clicked. ({}, {})".format(w.mouse_position_x, w.mouse_position_y))
-        if (450 <= w.mouse_position_x <= 750) and (400 <= w.mouse_position_y <= 500) and  config.game_started == 0:
-            d_print("Game Play Button Clicked.")
-            w.hideObject(w.data.image_main_button_play)
-            config.game_started = 1
+    check_play_button_clicked(w)
