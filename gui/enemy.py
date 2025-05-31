03ec9fe2b3fe664b559f01afef99254663d2e36f
hyungin0505 <hyungin0505@naver.com>
2025-06-01 01:00:40 +0900

    Edit: 경계에 enemy 생성되도록 하고, enemy.py에 디버깅용 함수 추가

--- gui/enemy.py
+++ gui/enemy.py
@@ -1,5 +1,6 @@
 import random
 from utils import *
+from core.debugger import d_print
 
 class Enemy:
     def __init__(self, w, x=0, y=0, speed=0, direction_x=0, direction_y=0):
@@ -9,24 +10,30 @@ class Enemy:
 
         edge = random.choice(['top', 'bottom', 'left', 'right'])
         if edge == 'top':
-            self.x = random.randint(0, SCREEN_WIDTH)
-            self.y = 0
+            self.x = random.randint(0-8, SCREEN_WIDTH-8)
+            self.y = 0-8
+            d_print("[Enemy Spawned] Enemy is on top. ({}, {})".format(self.x, self.y))
         elif edge == 'bottom':
-            self.x = random.randint(0, SCREEN_WIDTH)
-            self.y = SCREEN_HEIGHT
+            self.x = random.randint(0-8, SCREEN_WIDTH-8)
+            self.y = SCREEN_HEIGHT-8
+            d_print("[Enemy Spawned] Enemy is on bottom. ({}, {})".format(self.x, self.y))
         elif edge == 'left':
-            self.x = 0
-            self.y = random.randint(0, SCREEN_HEIGHT)
-        else:
-            self.x = SCREEN_WIDTH
-            self.y = random.randint(0, SCREEN_HEIGHT)
+            self.x = 0-8
+            self.y = random.randint(0-8, SCREEN_HEIGHT-8)
+            d_print("[Enemy Spawned] Enemy is on left. ({}, {})".format(self.x, self.y))
+        elif edge == 'right':
+            self.x = SCREEN_WIDTH-8
+            self.y = random.randint(0-8, SCREEN_HEIGHT-8)
+            d_print("[Enemy Spawned] Enemy is on right. ({}, {})".format(self.x, self.y))
 
         self.image = w.newImage(
             x=self.x,
             y=self.y,
             filename='./assets/images/enemy.png',
-            new_width=32,
-            new_height=32,
+            new_width=16,
+            new_height=16,
             isVisible=True,
             isPixelwiseModifiable=False,
         )
+    def move(self):
+        self.x += self.direction_x
