57bb66dc06120cd6cfda86d032dd7fb853b201d0
hyungin0505 <hyungin0505@naver.com>
2025-06-01 01:50:24 +0900

    Edit: 메인에서 아무데나 클릭하면 showObject(player) 실행되는 오류 수정

--- core/button.py
+++ core/button.py
@@ -4,8 +4,14 @@ from utils import config
 def check_play_button_clicked(w):
     if w.mouse_buttons[1]:
         d_print("Mouse 1 Clicked. ({}, {})".format(w.mouse_position_x, w.mouse_position_y))
-        if (450 <= w.mouse_position_x <= 750) and (400 <= w.mouse_position_y <= 500) and  config.game_started == 0:
+        if (w.getPosition(w.data.image_main_button_play)[0]
+            <= w.mouse_position_x
+            <= w.getPosition(w.data.image_main_button_play)[0]+w.getSize(w.data.image_main_button_play)[0]
+        ) and (w.getPosition(w.data.image_main_button_play)[1]
+               <= w.mouse_position_y
+               <= w.getPosition(w.data.image_main_button_play)[1]+w.getSize(w.data.image_main_button_play)[1]
+        ) and  config.game_started == 0:
             d_print("Game Play Button Clicked.")
             w.hideObject(w.data.image_main_button_play)
             config.game_started = 1
-        w.showObject(w.data.image_game_player)
+            w.showObject(w.data.image_game_player)
