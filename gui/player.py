b40d65197e809f74afbd7f336b4965ea372d9ffc
hyungin0505 <hyungin0505@naver.com>
2025-06-01 13:40:03 +0900

    Edit: 플레이어 위치 정중앙

--- gui/player.py
+++ gui/player.py
@@ -1,7 +1,7 @@
 def game_player_init(w):
     w.data.image_game_player = w.newImage(
-        x = (1200-32)/2-16,
-        y = (800-32)/2-16,
+        x = (1200-32)/2,
+        y = (800-32)/2,
         filename = './assets/images/player.png',
         new_width=32,
         new_height=32,
