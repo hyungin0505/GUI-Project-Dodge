5ccb7e73c75bc2601fd2de402724c3d088190150
hyungin0505 <hyungin0505@naver.com>
2025-06-01 12:55:21 +0900

    Edit: 난이도 조정, 게임 플레이 중 Enemy 수 표시

--- core/enemy.py
+++ core/enemy.py
@@ -10,8 +10,9 @@ def make_enemy(w, enemies):
         if random.randint(0, 10-config.enemy_spawn_tick) == 0:
             enemies.append(Enemy(w))
             config.enemy_count += 1
+            w.setTitle("Avoid Enemies!! Enemies: {}".format(config.enemy_count))
 
-    if config.enemy_count == config.difficulty:
+    if config.enemy_count >= config.difficulty:
         config.enemy_spawn_tick += 1
-        config.difficulty *= 2
+        config.difficulty *= 1.8
         d_print("[Game Config Update] Enemy Spawn Tick: {}, Difficlty: {}".format(config.enemy_spawn_tick, config.difficulty))
--- utils/config.py
+++ utils/config.py
@@ -3,10 +3,8 @@ SCREEN_HEIGHT = 800
 DEBUGGING = True
 
 game_started = 0
-
 difficulty = 10
-
 player_speed = 8
-
 enemy_count = 0
 enemy_spawn_tick = 1
+enemy_target_player = False
