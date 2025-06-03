f6c7b87f8a92ad35c93822cc7db6dc93bebb81f6
hyungin0505 <hyungin0505@naver.com>
2025-06-03 16:46:50 +0900

    Edit: enemy_spawn_tick이 10 이상일 떄 OOB 해결

--- core/enemy.py
+++ core/enemy.py
@@ -5,13 +5,14 @@ from gui.enemy_sprite import Enemy
 from utils import config
 
 def generate(w, enemies):
-    if config.game_started:
+    if config.game_started == 1 and config.enemy_spawn_tick != 10:
         if random.randint(0, 10-config.enemy_spawn_tick) == 0:
             enemies.append(Enemy(w))
             config.enemy_count += 1
             w.setTitle("Avoid Enemies!! Enemies: {}".format(config.enemy_count))
 
     if config.enemy_count >= config.difficulty:
-        config.enemy_spawn_tick += 1
+        if config.enemy_spawn_tick < 10:
+            config.enemy_spawn_tick += 1
         config.difficulty *= 1.8
         d_print("[Game Config Update] Enemy Spawn Tick: {}, Difficlty: {}".format(config.enemy_spawn_tick, config.difficulty))
