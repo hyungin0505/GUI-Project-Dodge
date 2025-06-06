e2c5e32d6ff9feb11f6582193cc50889dcc658a3
hyungin0505 <hyungin0505@naver.com>
2025-06-06 12:58:58 +0900

    Edit: 메인 화면 title 설정

--- scenes/main_screen.py
+++ scenes/main_screen.py
@@ -1,6 +1,7 @@
 from gui import background_sprite, play_button_sprite, logo_sprite
 
 def init(w):
+    w.setTitle("Press 'Enter' to start")
     background_sprite.init(w)
     logo_sprite.init(w)
     play_button_sprite.init(w)
