86ae971092b5cc2fc5e08cfc812ba14ee0b99917
hyungin0505 <hyungin0505@naver.com>
2025-06-26 03:01:48 +0900

    Refactor: enemy.generate 과도한 조건문 완화

--- core/game.py
+++ core/game.py
@@ -12,8 +12,8 @@ def init(w):
 def start(w, enemies):
     if config.game_started == 0 or config.game_started == 2:
         button.play(w)
-
-    player.move(w)
-    enemy.generate(w, enemies)
+    else:
+        player.move(w)
+        enemy.generate(w, enemies)
     for e in enemies:
-        e.move(w, enemies)
+            e.move(w, enemies)
